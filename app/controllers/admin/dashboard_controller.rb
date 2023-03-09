9aa332a63f7bf2c2a5a3d6ae0aa71aa5adaf0043
Can Kologlu <mckologlu@gmail.com>
2023-03-09 12:36:45 -0500

    add categories to the dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,6 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def show
     @products = Product.all.count
-    # @categories = Product.categories_id.count
+    @categories = Category.all.count
   end
 end
