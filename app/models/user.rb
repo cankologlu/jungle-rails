1be91bf911f5207c9b0ddba577d815b52613baa3
Can Kologlu <mckologlu@gmail.com>
2023-03-14 18:38:56 -0400

    rspec tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :password, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates :password, length: {minimum: 5, maximum: 15}
+
+  def self.authenticate_with_credentials(email, password)
+    stripped = email.strip.downcase
+    user = User.find_by(email: stripped)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
 end
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    before(:each) do
+    @user = User.new(name:"shadow", email:"shadow@shadow.com", 
+    password:"shadow", password_confirmation:"shadow")
+    end
+
+    it "checks if the user is valid" do
+      expect(@user).to be_valid
+    end
+
+    it "checks if the password and password_confirmation match"do
+    @user.password_confirmation = ""
+    expect(@user).to_not be_valid
+    expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "checks if the email is case sensitive and unique" do
+      user1 = User.create(name:"hello", email:"hello@hi.com", password:"12345")
+      user2 = User.create(name:"hey", email:"HeLLo@Hi.com", password:"12345")
+      expect(user2).to_not be_valid
+      expect(user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "checks if the password is more than 5 characters" do
+      @user.password = "1"
+      @user.password_confirmation = "1"
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end
+  end
+
+  describe ".authenticate_with_credentials" do
+    before(:each) do
+      @user = User.create(name:"shadow", email:"shadow@shadow.com", 
+      password:"shadow", password_confirmation:"shadow")
+    end
+
+    it "returns nil if the email is incorrect" do
+     result = User.authenticate_with_credentials("abo@ool.com", "password")
+     expect(result).to be_nil
+    end
+
+    it "returns nil if the password is incorrect" do
+      result = User.authenticate_with_credentials("shadow@shadow.com", "banana")
+      expect(result).to be_nil
+    end
+
+    it "returns the user if provided correct email and password" do
+      result = User.authenticate_with_credentials("shadow@shadow.com", "shadow")
+      expect(result).to be_an_instance_of(User)
+    end 
+
+    it "authenticates if user adds spaces before or after their email" do
+      result = User.authenticate_with_credentials(" shadow@shadow.com ", "shadow")
+      expect(result).to be_an_instance_of(User)
+    end
+
+    it "authenticates if user uses capital case entering their email" do
+      result = User.authenticate_with_credentials("ShaDOW@SHAdow.com", "shadow")
+      expect(result).to be_an_instance_of(User)
+    end
+
+  end
 end
