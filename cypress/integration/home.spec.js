f3bf038a02bbdaaa825dc19cdd8155989aef50a5
Can Kologlu <mckologlu@gmail.com>
2023-03-14 23:33:14 -0400

    db initilize setup

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,19 @@
+describe('jungle to open', () => { 
+  beforeEach(() => {
+
+
+
+    cy.visit('http://127.0.0.1:3000')
+  });
+  
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  // it("There is products on the page", () => {
+  // cy.get(".products article").should("be.visible");
+  // });
+
+
+})
